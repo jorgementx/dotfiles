6c94565f118e5dfeb18ef3c130bb0d5da19c2bdc
jorge-pb <jmentxaka@purpleblob.net>
2024-10-18 17:32:17 +0200

    feat: home-manager configuration updated for flakes support

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -7,13 +7,14 @@
   nixpkgs.config.allowunfree = true;
   nixpkgs.config.allowUnfreePredicate = _: true;
 
-  home = rec {
+  home = let
     username = "jorge";
+  in {
+    inherit username;
     homeDirectory = "/home/${username}";
     stateVersion = "24.05";
     packages = with.pkgs; [
       anki
-      cava
       obsidian
       vscode
       google-chrome
@@ -23,12 +24,13 @@
       pipx
       peco
       jq
+      jqp
     ];
     # Home Manager is pretty good at managing dotfiles. The primary way to manage
     # plain files is through 'home.file'.
     file = {};
-
     sessionVariables = {
+      VISUAL = "code";
       EDITOR = "vim";
     };
 
@@ -46,7 +48,23 @@
     };
   };
 
-  programs.mpv.enable = true;
+    programs.mpv = {
+    enable = true;
+    scriptOpts = {
+      subs2srs = {
+        autoclip="yes";
+        deck_name="Korean deck::Sentence Mining";
+        model_name="Refold Sentence Miner: Sentence";
+        sentence_field="Example Sentence";
+        secondary_field="Sentence Translation";
+        audio_field="sentence_audio";
+        image_field="image";
+        snapshot_quality=80;
+        vocab_field="Word";
+        vocab_audio_field="word_audio";
+      };
+    };
+  };
 
   programs.translate-shell = {
       enable = true;
@@ -227,6 +245,19 @@
       };
     };
   };
+
+
+  xdg.desktopEntries = {
+    mpv = {
+      type = "Application";
+      name = "mpv Media Player";
+      mimeType = ["application/ogg" "application/x-ogg" "application/mxf" "application/sdp" "application/smil" "application/x-smil" "application/streamingmedia" "application/x-streamingmedia" "application/vnd.rn-realmedia" "application/vnd.rn-realmedia-vbr" "audio/aac" "audio/x-aac" "audio/vnd.dolby.heaac.1" "audio/vnd.dolby.heaac.2" "audio/aiff" "audio/x-aiff" "audio/m4" "aaudio/x-m4a" "application/x-extension-m4a" "audio/mp1" "audio/x-mp1" "audio/mp2" "audio/x-mp2" "audio/mp3" "audio/x-mp3" "audio/mpeg" "audio/mpeg2" "audio/mpeg3" "audio/mpegurl" "audio/x-mpegurl" "audio/mpg" "audio/x-mpg" "audio/rn-mpeg" "audio/musepack" "audio/x-musepack" "audio/ogg" "audio/scpls" "audio/x-scpls" "audio/vnd.rn-realaudio" "audio/wav" "audio/x-pn-wav" "audio/x-pn-windows-pcm" "audio/x-realaudio" "audio/x-pn-realaudio" "audio/x-ms-wma" "audio/x-pls" "audio/x-wav" "video/mpeg" "video/x-mpeg2" "video/x-mpeg3" "video/mp4v-es" "video/x-m4v" "video/mp4" "application/x-extension-mp4" "video/divx" "video/vnd.divx" "video/msvideo" "video/x-msvideo" "video/ogg" "video/quicktime" "video/vnd.rn-realvideo" "video/x-ms-afs" "video/x-ms-asf" "audio/x-ms-asf" "application/vnd.ms-asf" "video/x-ms-wmv" "video/x-ms-wmx" "video/x-ms-wvxvideo" "video/x-avi" "video/avi" "video/x-flic" "video/fli" "video/x-flc" "video/flv" "video/x-flv" "video/x-theora" "video/x-theora+ogg" "video/x-matroska" "video/mkv" "audio/x-matroska" "application/x-matroska" "video/webm" "audio/webm" "audio/vorbis" "audio/x-vorbis" "audio/x-vorbis+ogg" "video/x-ogm" "video/x-ogm+ogg" "application/x-ogm" "application/x-ogm-audio" "application/x-ogm-video" "application/x-shorten" "audio/x-shorten" "audio/x-ape" "audio/x-wavpack" "audio/x-tta" "audio/AMR" "audio/ac3" "audio/eac3" "audio/amr-wb" "video/mp2t" "audio/flac" "audio/mp4" "application/x-mpegurl" "video/vnd.mpegurl" "application/vnd.apple.mpegurl" "audio/x-pn-au" "video/3gp" "video/3gpp" "video/3gpp2" "audio/3gpp" "audio/3gpp2" "video/dv" "audio/dv" "audio/opus" "audio/vnd.dts" "audio/vnd.dts.hd" "audio/x-adpcm" "application/x-cue" "audio/m3u" "audio/vnd.wave" "video/vnd.avi"];
+      terminal = false;
+      categories = ["AudioVideo" "Audio" "Video" "Player" "TV"];
+      exec = "mpv --player-operation-mode=pseudo-gui -- %U";
+    };
+  };
+
 
   nix.gc = {
     automatic = true;
